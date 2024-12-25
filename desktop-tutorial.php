a54e2c0c8d7ec68ad7d1e10cc5d0b7f5c88a2b1f
CaptainSubtle <65496763+Tafmakura@users.noreply.github.com>
2024-12-25 23:27:15 +0200

    Create desktop-tutorial.php

---
+++ desktop-tutorial.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Desktop Tutorial
+ * Description: Server integration and yield augmentation plugin for WooCommerce.
+ * Version: 1.0.0
+ * Author: Your Name
+ * Text Domain: siya
+ */
