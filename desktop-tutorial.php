2adc78d658917c323a1fc651da0e54aa6d03eb83
CaptainSubtle <65496763+Tafmakura@users.noreply.github.com>
2024-12-26 00:22:39 +0200

    Update desktop-tutorial.php

--- desktop-tutorial.php
+++ desktop-tutorial.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Desktop Tutorial
  * Description: Server integration and yield augmentation plugin for WooCommerce.
- * Version: 36.0.2
+ * Version: 100.0.2
  * Author: Your Name
  * Text Domain: siya
  */
